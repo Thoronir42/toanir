bd3f61114d79a8aada16dffd6c70f72aa3cc00cf
Toanir <Thoronir42@Gmail.com>
2025-08-08 08:59:52 +0200

    Add some rizz to the transcriber

--- src/routes/awoo/rift-transcribe/+page.svelte
+++ src/routes/awoo/rift-transcribe/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import "reboot.css/dist/reboot.css";
+    import "./transcribe.scss";
     import { goto } from "$app/navigation";
     import type { PageData } from "./$types";
     import { onMount } from "svelte";
@@ -13,7 +14,7 @@
     })
     ))
 
-    const encodeIndex: Record<string, typeof buttons[0]> = Object.fromEntries(buttons.map((button) => [button.natural, button]))
+    const encodeIndex: Record<string, typeof buttons[0]> = Object.fromEntries(buttons.map((button) => [button.label || button.natural, button]))
     let message = $state("")
 
     function pressButton(button: typeof data.buttons[0]) {
@@ -26,29 +27,78 @@
     function copyMessage() {
         navigator.clipboard.writeText(message)
     }
-    function buttonToImage(button: typeof data.buttons[0]): string {
+    function buttonToImage(button: typeof data.buttons[0]): string|null {
+        if (button.file === null || button.natural === null) return null
         return `/awoo/symbols/rift/${(button.file || button.natural + '.png')}`
     }
+    function formatStyle(button: typeof data.buttons[0], i?: number) {
+        if (!button.style) return undefined;
+        return Object.entries(button.style)
+            .map(([key, value]) => `--${key}: ${value};`)
+            .join(' ')
+            + (i ? `--i: ${i};` :  '')
+    }
+
     function switchMode(mode: string) {
         const url = new URL(window.location.toString())
         url.searchParams.set('mode', mode)
         goto(url, {replaceState: true})
     }
 
-    onMount(() => {
+    async function inverseImageUrl(url: string): Promise<string> {
+        const image = await new Promise<HTMLImageElement>((res, rej) => {
+            const image = new Image();
+            image.src = keyImageHref
+            image.onload = () => res(image)
+            image.onerror = (e) => rej(e)
+        })
+        const canvas = document.createElement("canvas")
+        canvas.width = image.width
+        canvas.height = image.height
+        const ctx = canvas.getContext('2d')!
+        ctx.drawImage(image, 0, 0)
+        const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+        for (let i = 0; i < imgData.data.length; i += 4) {
+            imgData.data[i] = 255 - imgData.data[i];
+            imgData.data[i + 1] = 255 - imgData.data[i + 1];
+            imgData.data[i + 2] = 255 - imgData.data[i + 2];
+            // imgData.data[i + 3] = 255; // keep alpha channel same
+        }
+        ctx.putImageData(imgData, 0, 0)
+        return canvas.toDataURL('jpg')
+    }
+    let keyImageHref = $state("/awoo/voidscript.jpg");
+    let keyImageInverse  = $state('');
+    onMount(async () => {
+        keyImageInverse = await inverseImageUrl(keyImageHref)
+        
         buttons.forEach((button) => {
+            if (!button.href) return;
+
             const image = new Image()
             image.onload = () => button.state = 'ready'
             image.onerror = () => button.state = 'error'
             image.src = button.href
-
         })
+
+        const styleEl = document.createElement("style");
+        styleEl.id = "transcriber-keys";
+        styleEl.appendChild(document.createTextNode(`
+.transcriber {
+    --key-image: url(${keyImageHref});
+    --key-image-inverse: url(${keyImageInverse});
+}
+        `))
+        document.head.appendChild(styleEl)
+        return () => {
+            styleEl.remove()
+        }
     })
 
 </script>
 
-<div class="container">
-    <h1>Rift transcriber v0.2</h1>
+<div class="container transcriber">
+    <h1>Rift transcriber v0.9</h1>
     <div class="controls radio-2">
         {#each data.modes as mode}
         <label>
@@ -60,12 +110,14 @@
         </label>
         {/each}
     </div>
-    <div class="keyboard" data-mode={data.mode}>
-        {#each buttons as button}
+    <div class="keyboard flex" data-mode={data.mode}>
+        {#each buttons as button, i}
         {#if button.natural}
-        <button aria-label={button.natural} data-key={button.natural}
-            onclick={() => pressButton(button)}>
-            {#if button.natural === 'back'}
+        <button class="key" aria-label={button.natural} data-key={button.natural}
+            onclick={() => pressButton(button)} style={formatStyle(button, i)}>
+            {#if button.style && data.mode === 'decode'}
+                <span></span>
+            {:else if button.natural === 'back'}
                 <span>⌫</span>
             {:else if data.mode === 'encode'}
                 <span class="natural">{button.label || button.natural}</span>
@@ -76,7 +128,7 @@
             {/if}
         </button>
         {:else}
-            <br>
+            <div role="separator"></div>
         {/if}
         {/each}
     </div>
@@ -88,15 +140,15 @@
 </div>
 {:else}
 <div class="encoded">
-    {#each message as letter}
-    {@const button = encodeIndex[letter]}
+    {#each message as letter, i}
+    {@const button = encodeIndex[letter.toLowerCase()]}
     {#if letter === '\n'}
         <br>
     {:else if !button || button.natural === ' '}
     <div class="key">{letter}</div>
     {:else}
-    <div class="key">
-        {@render glyph(button)}
+    <div class="key" style={formatStyle(button, i)}>
+        {@render glyph(button, true)}
     </div>
     {/if}
 
@@ -116,8 +168,10 @@
 
 </div>
 
-{#snippet glyph(button: typeof buttons[0])}
-    {#if button.state === 'ready'}
+{#snippet glyph(button: typeof buttons[0], justSpan: boolean = false)}
+    {#if justSpan}
+        <span></span>
+    {:else if button.state === 'ready'}
     <img src={button.href} alt="">
     {:else if button.state === 'error'}
     <span>❌</span>
@@ -126,109 +180,3 @@
     {/if}
 {/snippet}
 
-<style lang="scss">
-:global(body) {
-    background-color: hsl(275, 59%, 28%);
-    color: hsl(30, 95%, 53%);
-}
-:global(:is(input, textarea, button)) {
-    accent-color: #f98917;
-    background-color: hsl(275, 59%, 90%);
-}
-
-.controls {
-    padding: 0.25rem 1rem;
-    display: flex;
-    justify-content: center;
-    gap: 1rem;
-
-    label {
-        width: fit-content;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-
-        padding: 0.25rem;
-
-        background-color: #aaa8;
-    }
-
-    &.radio-2 {
-        > label {
-            gap: 0.5rem;
-        }
-        > label:first-child {
-            flex-direction: row-reverse;
-        }
-        > label:last-child {
-            flex-direction: row;
-        }
-    }
-}
-.keyboard {
-    width: fit-content;
-    margin: 0 auto;
-    text-align: center;
-    background-color: #aaa8;
-    padding: 0.5rem 0.2rem;
-
-    > button {
-        width: 1.75rem;
-        
-        margin: 0.1rem;
-
-        > img {
-            width: 100%;
-            height: auto;
-            aspect-ratio: 1;
-            overflow: hidden;
-        }
-
-        &[data-key=" "] {
-            width: 6rem;
-            height: 1.2rem;
-        }
-        
-    }
-
-    > br:nth-of-type(3) + button {
-        margin-inline-start: 1.5rem;
-    }
-
-    &[data-mode="decode"] > button {
-        padding: 0;
-    }
-}
-[name="message"] {
-    margin-block-start: 1rem;
-    width: 100%;
-    resize: vertical;
-    min-height: 20rem;
-}
-
-.encoded {
-    > .key {
-        display: inline-block;
-        width: 2em;
-        height: 2em;
-
-        > img {
-            width: 100%;
-            height: 100%;
-            object-fit: contain;
-        }
-    }
-}
-
-.credits {
-    margin-block-end: 2rem;
-    img {
-        display: block;
-        max-width: min(80vw, 100%);
-        height: auto;
-        margin: 0 auto;
-        border: 4px groove #f98917;
-    }
-}
-
-</style>
